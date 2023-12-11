6cbe7f63e757f3e2b633e457887279065fc1eb99
CrossDev <joancruz0502@hotmail.com>
2023-12-11 18:15:27 -0400

    test: mongo test

--- test/data/mongo/init.test.ts
+++ test/data/mongo/init.test.ts
@@ -1,14 +1,28 @@
 import { MongoDatabase } from '../../../src/data/mongo'
+import mongoose from 'mongoose';
 
 
 
 describe('init MongoDB', () => {
 
+    afterAll(() => {
+        mongoose.connection.close()
+    })
 
     test('should connect to MongoDB', async () => {
 
-        await MongoDatabase.connect({ mongoURL: process.env.MONGO_URI, dbName: process.env.MONGO_DB_NAME });
-        console.log(process.env.MONGO_URI, process.env.MONGO_DB_NAME);
+        const connect = await MongoDatabase.connect({ mongoURL: process.env.MONGO_URI!, dbName: process.env.MONGO_DB_NAME! });
 
+        expect(connect).toBeTruthy();
+        expect(connect).toBe(true);
+    })
+
+    test('should throw an error', async () => {
+        try {
+            const connect = await MongoDatabase.connect({ mongoURL: 'mongodb://alumnno:123456@10.0.0.3:27017/?authMechanism=DEFAULT', dbName: process.env.MONGO_DB_NAME! });
+            expect(connect).toBe(false);
+        } catch (error) {
+            expect(error).toBeTruthy();
+        }
     })
 })
